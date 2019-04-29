f823ae02ad3412912f360cbfd5fcd0826f7b98a1
Francesco Rodriguez <ferc26899@gmail.com>
2019-04-29 15:22:46 -0430

    init project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome Conex',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome Conex'),
+        ),
+        body: Center(
+          child: Text('Hola Yotman'),
+        ),
+      ),
+    );
+  }
+}
